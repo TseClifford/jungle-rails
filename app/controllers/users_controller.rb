adb4d3804e03e2a030edc9ae6167d41a69d50318
Clifford Tse <TseClifford@gmail.com>
2022-03-10 22:44:46 -0500

    fix permit email bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
+      puts user.id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -15,7 +16,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-  params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation)
+  params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 
 end
